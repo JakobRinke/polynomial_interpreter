f39829dc4e55aba5e9341d21d96c4f4625375121
JakobRinke <61539310+JakobRinke@users.noreply.github.com>
2023-12-18 12:26:23 +0100

    fix

--- main.py
+++ main.py
@@ -1,16 +1,99 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+from polynom import Polynom
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def creationTest():
+    print("Creation test:")
+    p = Polynom([1, 2, 3])
+    print(p)
+    print(p(0))
+    print(p(2))
+
+def additionTest():
+    print("Addition test:")
+    p1 = Polynom([1, 2, 3])
+    p2 = Polynom([1, 2, 3])
+    print(p1 + p2)
+
+def subtractionTest():
+    print("Subtraction test:")
+    p1 = Polynom([2, 3, 4])
+    p2 = Polynom([1, 2, 3])
+    print(p1 - p2)
+
+def negationTest():
+    print("Negation test:")
+    p1 = Polynom([2, 3, 4])
+    print(-p1)
+
+def multiplicationTest():
+    print("Multiplication test:")
+    p1 = Polynom([2, 1])
+    print(p1 * p1)
+    print(p1 * 2)
+
+def powerTest():
+    print("Power test:")
+    p1 = Polynom([2, 1])
+    print(p1 ** 2)
+
+def equalityTest():
+    print("Equality test:")
+    p1 = Polynom([2, 1])
+    p2 = Polynom([2, 1])
+    print(p1 == p2)
+    p3 = Polynom([2, 1, 5])
+    print(p1 == p3)
+    print(p1 != p3)
+    p4 = Polynom([2, 5])
+    print(p1 == p4)
+
+def derivativeTest():
+    print("Derivative test:")
+    p1 = Polynom([4, 2, 3])
+    print(p1.get_derivative())
 
 
-# Press the green button in the gutter to run the script.
+def divisionTest():
+    print("Division test:")
+    p1 = Polynom([4, 2, 3])
+    p2 = Polynom([2, 1])
+    print(p1 // p2)
+    print(p1 % p2)
+
+def solveTest():
+    print("Solve test:")
+
+    p1 = Polynom([-3, -6, -1,  4, 2])
+    print(p1)
+    print(p1.solve())
+
+    p2 = Polynom([1, 2, 1, -5])
+    print(p2)
+    print(p2.solve())
+
+    p3 = Polynom([1, -4, 1])
+    print(p3)
+    print(p3.solve())
+
+    p4 = Polynom([1, 4, 2, -1, -2, 2])
+    print(p4)
+    print(p4.solve())
+
+
+
+
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    creationTest()
+    additionTest()
+    subtractionTest()
+    negationTest()
+    multiplicationTest()
+    powerTest()
+    equalityTest()
+    derivativeTest()
+    divisionTest()
+    solveTest()
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
--- polynom.py
+++ polynom.py
@@ -1,28 +1,155 @@
-
+import numbers
 
 
 class Polynom:
 
     def __init__(self, coefficients):
+        for i in range(len(coefficients)-1, 0, -1):
+            if coefficients[i] == 0:
+                coefficients.pop(i)
+            else:
+                break
         self.coefficients = coefficients
 
     def calc(self, x):
         y = 0
         for i in range(len(self.coefficients)):
-            y += x ** i + self.coefficients[i]
+            y += x ** i * self.coefficients[i]
         return y
 
+    def __call__(self, x):
+        return self.calc(x)
+
     def get_coef(self, i):
-        if 0 < i < len(self.coefficients):
+        if 0 <= i < len(self.coefficients):
             return self.coefficients[i]
         return 0
 
-    def get_derivative(self, i):
+    def get_derivative(self):
         coeffs_deri = [0] * (len(self.coefficients) - 1)
-        for i in range(len(self.coefficients)):
-            coeffs_deri[i] = self.coefficients[i] * i
+        for i in range(len(coeffs_deri)):
+            coeffs_deri[i] = self.coefficients[i+1] * (i+1)
         return Polynom(coeffs_deri)
 
+    def solve(self):
+        if len(self.coefficients) == 1:
+            return []
+        if len(self.coefficients) == 2:
+            return [-self.coefficients[0] / self.coefficients[1]]
+        if len(self.coefficients) == 3:
+            a = self.coefficients[2]
+            b = self.coefficients[1]
+            c = self.coefficients[0]
+            delta = b ** 2 - 4 * a * c
+            if delta < 0:
+                return []
+            if delta == 0:
+                return [-b / (2 * a)]
+            return [(-b - delta ** 0.5) / (2 * a), (-b + delta ** 0.5) / (2 * a)]
+
+        # Use Newton's method
+        x = 0
+        deri = self.get_derivative()
+        y = self(x)
+        n = 4000
+        while abs(y) > 1e-10:
+            x = x - y / deri(x)
+            y = self(x)
+            n-=1
+            if n <= 0:
+                return []
+        x = round(x, 4)
+        return list(set([x] + (self // Polynom([-x, 1])).solve()))
+
+
+
     def __add__(self, other):
-        co = [self.get_coef(i) + other.coef(i) for i in range(max(len(self.coefficients), len(other.coefficient)))]
+        co = [self.get_coef(i) + other.get_coef(i) for i in range(max(len(self.coefficients), len(other.coefficients)))]
         return Polynom(co)
+
+    def __sub__(self, other):
+        co = [self.get_coef(i) - other.get_coef(i) for i in range(max(len(self.coefficients), len(other.coefficients)))]
+        return Polynom(co)
+
+    def __neg__(self):
+        co = [-self.get_coef(i) for i in range(len(self.coefficients))]
+        return Polynom(co)
+
+    def __mul__(self, other):
+        if type(other) != type(self):
+            if type(other) == int or type(other) == float:
+                return Polynom([self.coefficients[i] * other for i in range(len(self.coefficients))])
+            return NotImplemented
+        co = [0] * (len(self.coefficients) + len(other.coefficients) - 1)
+        for i in range(len(self.coefficients)):
+            for j in range(len(other.coefficients)):
+                co[i + j] += self.coefficients[i] * other.coefficients[j]
+        return Polynom(co)
+
+    def __rmul__(self, other):
+        co = [0] * len(self.coefficients)
+        for i in range(len(self.coefficients)):
+            co = self.coefficients[i] * other
+        return co
+
+    def __floordiv__(self, other):
+        if type(other) != type(self):
+            if type(other) == int or type(other) == float:
+                return Polynom([self.coefficients[i] // other for i in range(len(self.coefficients))])
+            return NotImplemented
+        if len(self.coefficients) < len(other.coefficients):
+            return Polynom([0])
+        # Use Polynomial long division, dont change self
+        coeff_self = self.coefficients.copy()
+        co = [0] * (len(self.coefficients) - len(other.coefficients) + 1)
+        for i in range(len(self.coefficients) - len(other.coefficients), -1, -1):
+            co[i] = coeff_self[i + len(other.coefficients) - 1] // other.coefficients[-1]
+            for j in range(len(other.coefficients)):
+                coeff_self[i + j] -= co[i] * other.coefficients[j]
+        return Polynom(co)
+
+
+    def __mod__(self, other):
+        if type(other) != type(self):
+            if type(other) == int or type(other) == float:
+                return Polynom([self.coefficients[i] % other for i in range(len(self.coefficients))])
+            return NotImplemented
+        if len(self.coefficients) < len(other.coefficients):
+            return Polynom(self.coefficients)
+        # Use Polynomial long division
+        coeff_self = self.coefficients.copy()
+        co = [0] * (len(self.coefficients) - len(other.coefficients) + 1)
+        for i in range(len(self.coefficients) - len(other.coefficients), -1, -1):
+            co[i] = coeff_self[i + len(other.coefficients) - 1] // other.coefficients[-1]
+            for j in range(len(other.coefficients)):
+                coeff_self[i + j] -= co[i] * other.coefficients[j]
+        return Polynom(coeff_self)
+
+    def __truediv__(self, other):
+        return (self.__floordiv__(other), self.__mod__(other))
+
+    def __pow__(self, n):
+        if n < 0:
+            raise ValueError("n must be greater than 0")
+        if n == 0:
+            return Polynom([1])
+        if n > 0:
+            return self * self ** (n - 1)
+
+    def __eq__(self, other):
+        if len(self.coefficients) != len(other.coefficients):
+            return False
+        for i in range(len(self.coefficients)):
+            if self.coefficients[i] != other.coefficients[i]:
+                return False
+        return True
+
+    def __ne__(self, other):
+        return not self.__eq__(other)
+
+    def __str__(self):
+        s = ""
+        for i in range(len(self.coefficients)-1, 0, -1):
+            s += str(self.coefficients[i]) + "x^{" + str(i) + "} + "
+        s += str(self.coefficients[0])
+        return s
